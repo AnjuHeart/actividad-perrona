43b9251575d09bdc909acfdf848cd903064b5cfc
Yhunn <jybrant.efren@gmail.com>
2022-09-29 07:54:39 -0600

    flutter initialization

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BlocApplication());
+}
+
+class BlocApplication extends StatelessWidget {
+  const BlocApplication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
